6409aaa8546e330c5433e3561375e737fb31f015
juan <anarkigotic@gmail.com>
2019-04-11 13:58:39 -0500

    segundo commit practica jenkis

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ class Main {
     public static void main(String[] args){
         System.out.println("Hola mundo desde java");
         System.out.println("La conexion con githab con exito");
+        System.out.println("La conexion con githab con exito 2");
     }
 }
